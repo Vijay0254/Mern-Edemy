b636cc26fd3159cf89ccc82b8db76c49b54cb5c6
Vijay0254 <vijayabijeet254@gmail.com>
2025-03-19 09:57:42 +0530

    Removed cors properties

--- Backend/index.js
+++ Backend/index.js
@@ -9,11 +9,7 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT
 
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-}))
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
